594b8427be8f3c7214d6846658d49b6df49bc657
Kamil Jamroz <nergydev@empyrean.games>
2020-03-21 16:47:08 +0100

    win logic

--- lab2-tic-tac-toe/src/index.js
+++ lab2-tic-tac-toe/src/index.js
@@ -7,6 +7,37 @@ class Grid {
             this.cells.push(cell);
         }
     }
+    CheckWin() {
+        for (let i = 0; i < 3; i++) {
+            let cell1 = this.cells[i].content;
+            let cell2 = this.cells[i + 3].content;
+            let cell3 = this.cells[i + 6].content;
+            if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+                return true;
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            let cell1 = this.cells[i * 3].content;
+            let cell2 = this.cells[i + 3 + 1].content;
+            let cell3 = this.cells[i + 3 + 2].content;
+            if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+                return true;
+            }
+        }
+        let cell1 = this.cells[0].content;
+        let cell2 = this.cells[4].content;
+        let cell3 = this.cells[8].content;
+        if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+            return true;
+        }
+        cell1 = this.cells[3].content;
+        cell2 = this.cells[4].content;
+        cell3 = this.cells[6].content;
+        if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+            return true;
+        }
+        return false;
+    }
 }
 class Cell {
     constructor(grid, index, htmlElement) {
@@ -14,23 +45,22 @@ class Cell {
         this.index = index;
         this.content = CellContent.Empty;
         this.htmlElement = htmlElement;
-        this.htmlElement.onclick = () => {
-            this.HandleClick();
-        };
+        this.htmlElement.onclick = () => this.HandleClick();
     }
     HandleClick() {
-        if (turn === Turn.Player1) {
-            if (this.content == CellContent.Empty) {
+        if (this.content == CellContent.Empty) {
+            if (turn === Turn.Player1) {
                 this.htmlElement.innerHTML = "O";
                 this.content = CellContent.Circle;
-                SwitchTurn(turn);
             }
-        }
-        else {
-            if (this.content == CellContent.Empty) {
+            else {
                 this.htmlElement.innerHTML = "X";
                 this.content = CellContent.Cross;
-                SwitchTurn(turn);
+            }
+            SwitchTurn(turn);
+            let win = this.grid.CheckWin();
+            if (win === true) {
+                alert("Winner: " + turn);
             }
         }
     }
--- lab2-tic-tac-toe/src/index.ts
+++ lab2-tic-tac-toe/src/index.ts
@@ -11,6 +11,48 @@ class Grid {
             this.cells.push(cell);
         }
     }
+
+    CheckWin(): boolean {
+
+        // horizontal check
+        for (let i = 0; i < 3; i++) {
+            let cell1 = this.cells[i].content
+            let cell2 = this.cells[i+3].content
+            let cell3 = this.cells[i+6].content
+
+            if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+                return true
+            }
+        }
+
+        // vertical check
+        for (let i = 0; i < 3; i++) {
+            let cell1 = this.cells[i*3].content
+            let cell2 = this.cells[i+3 + 1].content
+            let cell3 = this.cells[i+3 + 2].content
+
+            if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+                return true
+            }
+        }
+
+        // diagonal check
+        let cell1 = this.cells[0].content
+        let cell2 = this.cells[4].content
+        let cell3 = this.cells[8].content
+        if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+            return true
+        }
+
+        cell1 = this.cells[3].content
+        cell2 = this.cells[4].content
+        cell3 = this.cells[6].content
+        if (cell1 === cell2 && cell2 === cell3 && cell1 != CellContent.Empty) {
+            return true
+        }
+
+        return false
+    }
 }
 
 class Cell {
@@ -25,23 +67,23 @@ class Cell {
         this.content = CellContent.Empty
 
         this.htmlElement = htmlElement;
-        this.htmlElement.onclick = () => {
-            this.HandleClick()
-        }
+        this.htmlElement.onclick = () => this.HandleClick();
     }
 
     HandleClick() {
-        if (turn === Turn.Player1) {
-            if (this.content == CellContent.Empty) {
+        if (this.content == CellContent.Empty) {
+            if (turn === Turn.Player1) {
                 this.htmlElement.innerHTML = "O"
                 this.content = CellContent.Circle
-                SwitchTurn(turn)
-            }
-        } else {
-            if (this.content == CellContent.Empty) {
+            } else {
                 this.htmlElement.innerHTML = "X"
                 this.content = CellContent.Cross
-                SwitchTurn(turn)
+            }
+
+            SwitchTurn(turn)
+            let win = this.grid.CheckWin()
+            if (win === true) {
+                alert("Winner: " + turn)
             }
         }
     }
